e85a0c0288204cabc4dc116bcf7a838956773b7a
ashleygraf101 <ashley.graf101@gmail.com>
2020-06-08 13:12:37 +1000

    learning cypress clean commit writing first e2e test

---
+++ cypress/integration/init.spec.js
@@ -0,0 +1,34 @@
+it('visits the app', () => {
+  cy.visit('https://saytheirname.netlify.app/')
+})
+
+it('displays list of 12 victims on the home page', () => {
+  cy.wait(3000)
+  cy.get('.profile-preview-container')
+    .should('have.length', 12)
+})
+
+it('find Jamel Floyd + verify that alt tags exist', () => {
+  cy.get('[alt="Jamel Floyd"]')
+    .should('have.length', 1)
+})
+
+it('click Jamel Floyd', () => {
+  cy.get('[alt="Jamel Floyd"]').click()
+})
+
+it("click link to Jamel Floyd's justice fund",  () => {
+  cy.get('[type="button"]:first').click({ force: true })
+  cy.get('h2:first')
+  .should('contain.text', 'Donate')
+}) 
+
+
+it('404 appears for bad routes', () => {
+  cy.visit('https://saytheirname.netlify.app/some/bad/route')
+  cy.get('h1')
+  	.should('have.text', '404')
+})
+
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -16,3 +16,15 @@ test('renders Home Page', () => {
 
   expect(getByText(/Delayed justice is injustice/i)).toBeInTheDocument();
 });
+
+
+test('landing on a bad page shows 404 page', () => {
+  const history = createMemoryHistory();
+  history.push('/some/bad/route');
+  const { container } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  expect(container.innerHTML).toHaveTextContent('Not all those who wander are lost, but it seems you may have taken a wrong turn.');
+});
